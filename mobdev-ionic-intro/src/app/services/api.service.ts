71ad8b590ea60084fa7d969b1d6ef18ab470084d
snap-balance <2022141@student.cct.ie>
2022-10-21 15:59:58 +0000

    up to api service

--- mobdev-ionic-intro/src/app/services/api.service.ts
+++ mobdev-ionic-intro/src/app/services/api.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+	
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getFilms() {
+    return this.http.get('https://swapi.dev/api/films');
+}
+getFilm(id) {
+  return this.http.get(`https://swapi.dev/api/films/${id}`);
+}
 }
